ef8bbb62774238f693dcfeb8ebadfc43c571cf58
K Sheenan <kaynesheenan+git@gmail.com>
2021-05-19 14:02:13 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const texts = [ 'web developer', 'house music dj', 'cloud specialist', 'cyclist', 'coffee lover'  ];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+if(count === texts.length) {
+    count=0;
+} 
+currentText = texts[count]
+letter=currentText.slice(0, ++index);
+// select the class and change its content to a letter
+document.querySelector('.txt-type').textContent = letter; 
+// As soon as letter length is the same as our currentText, it changes to the next one
+if(letter.length === currentText.length){
+    count++;
+// reset starting from the beginning of the word
+    index = 0;
+
+}
+setTimeout(type, 300);
+}) ();
